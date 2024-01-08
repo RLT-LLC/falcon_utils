435eaa5d850e7e70488a25982b6fa0e9f36e7776
xivteen <bvmarkovsk@gmail.com>
2024-01-08 13:37:54 +0300

    chore: added default stdout logger

--- logs/__init__.py
+++ logs/__init__.py
@@ -1,8 +1,11 @@
+import sys
 import logging
 from logging.handlers import RotatingFileHandler
 from datetime import datetime
 import os
 from pathlib import Path
+
+default_formatter = logging.Formatter("%(asctime)s | %(message)s", datefmt='%m/%d/%Y %H:%M:%S')
 
 
 def _ensure_logs_folder_exists():
@@ -14,11 +17,16 @@ def _ensure_logs_folder_exists():
 
 def declare_logger(service_name):
     _ensure_logs_folder_exists()
-    formatter = logging.Formatter("%(asctime)s | %(message)s", datefmt='%m/%d/%Y %H:%M:%S')
     handler = RotatingFileHandler(F'./logs/{service_name}_{datetime.now().strftime("%d.%m.%Y")}.log',
                                   maxBytes=204800, encoding="utf-8")
-    handler.setFormatter(formatter)
+    handler.setFormatter(default_formatter)
     logger = logging.getLogger(service_name)
     logger.addHandler(handler)
     logger.setLevel(logging.DEBUG)
     return logger
+
+
+def add_sys_output_handler(logger: logging.Logger):
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(default_formatter)
+    logger.addHandler(handler)
